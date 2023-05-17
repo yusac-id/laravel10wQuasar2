4cd3447a00572b8bdca382ee8a12920778d887a1
Yusac <yusac@vervans.com>
2023-05-17 12:21:12 +0700

    first commit

---
+++ resources/views/fe.blade.php
@@ -0,0 +1,5 @@
+@php
+    // This file must be stored inside Laravel resources/views/app.blade.php
+    // this include_once resolves the import from Laravel public folder
+    @include_once 'fe/index.html';
+@endphp
